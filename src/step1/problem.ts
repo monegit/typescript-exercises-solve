f6df694225cf2e9dc973b385ccb82547b1cc3eca
Jongwon Park <pjw970220@gmail.com>
2023-02-14 03:22:13 +0900

    remove unuseless summary description

--- src/step1/problem.ts
+++ src/step1/problem.ts
@@ -1,65 +1,4 @@
 /*
-
-Welcome to:
-
-    ................................................................
-    .                                                              .
-    .     ####################    ####################      E      .
-    .     ####################    ####################      X      .
-    .             ####            ####                      E      .
-    .             ####            ####                      R      .
-    .             ####            ####################      C      .
-    .             ####                            ####      I      .
-    .             ####                            ####      S      .
-    .             ####            ####################      E      .
-    .             ####            ####################      S      .
-    .                                                              .
-    ................................................................
-
-    The goal: Let everyone play with many different TypeScript features
-    and get an overview of TypeScript capabilities and principles.
-
-    Things to cover:
-
-        1. Basic typing.
-        2. Refining types.
-        3. Union types.
-        4. Merged types.
-        5. Generics.
-        6. Type declarations.
-        7. Module augmentation.
-        8. Advanced type mapping.
-
-    Rules and principles:
-
-        1. Avoid using "any" type at all costs.
-        2. Difficulty quickly grows one exercise after another.
-        3. Feel free to send pull-requests if you've come up
-           with improvements!
-        4. Provide feedback to the creator of these exercises.
-        5. Enjoy.
-
-Brief UI guide:
-
-    +--------------------------------------------------------------+
-    | TypeScript exercises                                         |
-    +--------------------------------------------------------------+
-    | Exercises 1·2·3·4...   << Navigate through exercises >>      |
-    +---------------+----------------------------------------------+
-    | Files         | file.ts   << Filename and status >>          |
-    +---------------+----------------------------------------------+
-    | file.ts       | 1  import {x} from 'y';                      |
-    | dir           | 2                                            |
-    |   sub.ts      | 3                                            |
-    |               |                                              |
-    | << Current    |   << Currently selected file code editor >>  |
-    | exercise file |                                              |
-    | structure >>  +----------------------------------------------+
-    |               |                                              |
-    |               |   << Errors to fix in order to proceed >>    |
-    |               |                                              |
-    +---------------+----------------------------------------------+
-
 Intro:
 
     We are starting a small community of users. For performance
@@ -73,7 +12,6 @@ Intro:
 Exercise:
 
     Given the data, define the interface "User" and use it accordingly.
-
 */
 
 export type User = unknown;
